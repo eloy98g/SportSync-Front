18b9591c963243eb030da07900b88ef0a2cf892c
Eloy Gómez García <eloygomezgarcia@MacBook-Air-de-Eloy.local>
2024-03-12 12:52:47 +0100

    extended info added

--- src/api/placeholders/USER_2.ts
+++ src/api/placeholders/USER_2.ts
@@ -5,11 +5,16 @@ export default {
   phone: 9876543210,
   image: "https://www.conpaas.org/wp-content/uploads/2016/06/team-2.jpg",
   description: "Web developer and adventurer",
-  verified: true,
   playArea: {
     lat: 34.0522,
     lng: -118.2437,
     radius: 15,
   },
   birthDate: 819849600,
+  phoneVerified: true,
+  emailVerified: false,
+  creationDate: 946684800000,
+  published: 10,
+  participated: 23,
+  lastParticipation: 946684800000,
 };
