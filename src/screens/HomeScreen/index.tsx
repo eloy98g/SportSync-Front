07b2e2f8cf703fbc3423e1542596fcea39f3514f
Eloy Gómez García <eloygomezgarcia@MacBook-Air-de-Eloy.local>
2024-03-17 18:54:23 +0100

    version added

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -14,6 +14,7 @@ import SECTIONS from "./sections";
 
 // Theme
 import { PHONE } from "../../theme/breakPoints";
+import Version from "../../components/Version";
 
 const HomeScreen = () => {
   return (
@@ -31,7 +32,9 @@ const HomeScreen = () => {
               <HomeSection title={section.title} data={section.data} />
             </React.Fragment>
           ))}
-          <Divider height={100} />
+           <Divider height={50} />
+          <Version />
+          <Divider height={80} />
         </ScrollView>
       </View>
     </Screen>
