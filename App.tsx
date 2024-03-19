edbb0df59385235d5fa79d2d741dda06cf531b9c
Eloy Gómez García <eloygomezgarcia@MacBook-Air-de-Eloy.local>
2024-03-19 13:00:49 +0100

    share logic added_

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
 import { useEffect } from "react";
+import * as Linking from "expo-linking";
 import { Provider } from "react-redux";
+import { useNavigation } from "@react-navigation/native";
 import { PersistGate } from "redux-persist/integration/react";
+import { PortalProvider } from "tamagui";
 
 // Navigator
 import AppNavigator from "./src/navigation/AppNavigator";
@@ -10,16 +13,30 @@ import store, { persistor } from "./src/store/store";
 
 // Theme
 import TamaguiUI from "./src/theme/tamagui/TamaguiProvider";
-import { PortalProvider } from "tamagui";
 import { loadFonts } from "./src/theme/fonts";
 
 export default function App() {
+  const navigation = useNavigation();
   useEffect(() => {
     loadFonts();
   }, []);
 
+  useEffect(() => {
+    const handleDeepLink = ({ url }: any) => {
+      const { path, queryParams } = Linking.parse(url);
+
+      if (path === "sportup" && queryParams?.gid) {
+        navigation.navigate(
+          "ActivityDetail" as never,
+          { gid: queryParams.gid } as never
+        );
+      }
+    };
+
+    Linking.addEventListener("url", handleDeepLink);
+  }, []);
+
   return (
-   
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <TamaguiUI>
