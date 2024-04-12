23bb0eaeaa0e8d5a871da6c8dd63afbbf49c5d94
Eloy Gómez García <eloygomezgarcia@MacBook-Air-de-Eloy.local>
2024-04-12 18:47:01 +0200

    minor social bug fix

--- src/store/features/friends/friendsSlice.ts
+++ src/store/features/friends/friendsSlice.ts
@@ -32,7 +32,6 @@ const friendsSlice = createSlice({
     followPlayer: (state, action: PayloadAction<Player>) => {
       // TODO: API call for unfollowing a player
       const newArray = [...state.friends, action.payload];
-      console.log('newArray',newArray.length)
       state.friends = newArray;
     },
   },
