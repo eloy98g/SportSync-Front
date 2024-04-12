89a6b05022ebf8bbbfb8ed776b53570ecfbc1db3
Eloy Gómez García <eloygomezgarcia@MacBook-Air-de-Eloy.local>
2024-04-12 14:08:36 +0200

    friends slice added

---
+++ src/api/placeholders/FRIENDS.ts
@@ -0,0 +1,21 @@
+import Player from "../../store/types/activity/Player";
+
+const data: Player[] = [
+  {
+    gid: 2,
+    name: "Mary Pond",
+    image: "https://www.conpaas.org/wp-content/uploads/2016/06/team-2.jpg",
+  },
+  {
+    gid: 6,
+    name: "David Tennant",
+    image: "https://www.conpaas.org/wp-content/uploads/2016/06/team-1.jpg",
+  },
+  {
+    gid: 7,
+    name: "Lola Gomez",
+    image: "https://www.conpaas.org/wp-content/uploads/2016/06/team-2.jpg",
+  },
+];
+
+export default data;
