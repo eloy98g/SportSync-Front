6dc79caf3e9df239a0d1fca45704bcd91cee19a5
Eloy Gómez García <eloygomezgarcia@MacBook-Air-de-Eloy.local>
2024-05-04 18:48:24 +0200

    sport icon bug fix!

--- src/screens/user/ProfileScreen/components/ProfileHeader/ProfileImage.tsx
+++ src/screens/user/ProfileScreen/components/ProfileHeader/ProfileImage.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { useWindowDimensions, StyleSheet, Image } from "react-native";
 
-const ProfileImage = ({ image }: any) => {
+interface Props {
+  image: string;
+}
+const ProfileImage = ({ image }: Props) => {
   const width = useWindowDimensions().width;
+  const uri =
+    image || "https://cdn-icons-png.flaticon.com/512/10337/10337609.png";
   return (
     <Image
       style={[styles.image, { left: width * 0.5 - 60 }]}
-      source={{ uri: image || "https://cdn-icons-png.flaticon.com/512/10337/10337609.png" }}
+      source={{
+        uri,
+      }}
     />
   );
 };
